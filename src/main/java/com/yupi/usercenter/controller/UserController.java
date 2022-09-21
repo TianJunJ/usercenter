6eb70f85560128398a59670ac08f681abc6c3ecf
TianJunj <2792505987@qq.com>
2022-09-21 11:11:24 +0800

    fix: 摆烂小子

--- src/main/java/com/yupi/usercenter/controller/UserController.java
+++ src/main/java/com/yupi/usercenter/controller/UserController.java
@@ -32,65 +32,80 @@ public class UserController {
 
     /**
      * [@RequestBody]这个注解可以让前端与这个对象进行关联
+     *
      * @param userRegisterRequest
      * @return
      */
     @PostMapping("/register")
     public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
 
-        if(userRegisterRequest==null){
-            return  null;
+        if (userRegisterRequest == null) {
+            return null;
         }
         String userAccount = userRegisterRequest.getUserAccount();
         String userPassword = userRegisterRequest.getUserPassword();
         String checkPassword = userRegisterRequest.getCheckPassword();
-        if(StringUtils.isAnyBlank(userAccount,userPassword,checkPassword)){
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
             return null;
         }
 
         //可以下载 auto filling的一个插件，能够自动帮助补充参数 alt+enter
         //long id = userService.userRegister(userRegisterRequest, userPassword, checkPassword);
-        return userService.userRegister(userAccount,userPassword,checkPassword);
+        return userService.userRegister(userAccount, userPassword, checkPassword);
     }
 
     @PostMapping("/login")
     public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
 
-        if(userLoginRequest==null){
-            return  null;
+        if (userLoginRequest == null) {
+            return null;
         }
         String userAccount = userLoginRequest.getUserAccount();
         String userPassword = userLoginRequest.getUserPassword();
-        if(StringUtils.isAnyBlank(userAccount,userPassword)){
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
             return null;
         }
 
         //可以下载 auto filling的一个插件，能够自动帮助补充参数 alt+enter
         //long id = userService.userRegister(userRegisterRequest, userPassword, checkPassword);
-        return userService.userLogin(userAccount,userPassword,request);
+        return userService.userLogin(userAccount, userPassword, request);
+    }
+
+    @GetMapping("/current")
+    public User getCurrentUser(HttpServletRequest request) {
+        Object userObject = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObject;
+        if (currentUser == null) {
+            return null;
+        }
+
+        //此处有bug ，假如用户的状态为封号状态，下面的代码是无法检测的，因此可以在未来做优化
+        //todo 校验用户是否合法
+        long userId = currentUser.getId();
+        User user = userService.getById(userId);
+        return userService.getSafetyUser(user);
     }
 
     @GetMapping("/search")
-    public List<User> searchUser(String username , HttpServletRequest request){
+    public List<User> searchUser(String username, HttpServletRequest request) {
         /**
          * 下方代码完成了用户查询功能的实现
          * 由于功能比较简单，因此不像上面一样在userService中单独完成方法的实现
          * 在此处为偷懒了，正式的话还是要向上面一样单独曲线一个方法
          */
-        if(!isAdmin(request))
-        {
+        if (!isAdmin(request)) {
             return new ArrayList<>();
         }
 
-        QueryWrapper<User> queryWrapper=new QueryWrapper<>();
-         if(StringUtils.isNotBlank(username)){
-             queryWrapper.like("username",username);
-         }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(username)) {
+            queryWrapper.like("username", username);
+        }
         List<User> userList = userService.list(queryWrapper);
         /**
          * 下发的代码将服务器传回的密码设为空，避免信息泄流
-          */
-        return userList.stream().map(user->{
+         */
+        return userList.stream().map(user -> {
             user.setUserPassword(null);
             return userService.getSafetyUser(user);
         }).collect(Collectors.toList());
@@ -98,17 +113,17 @@ public class UserController {
 
     /**
      * 逻辑删除的方法
+     *
      * @param id
      * @return
      */
     @PostMapping("/delete")
-    public boolean deleteUser(@RequestBody long id, HttpServletRequest request){
+    public boolean deleteUser(@RequestBody long id, HttpServletRequest request) {
 
-        if(!isAdmin(request)){
+        if (!isAdmin(request)) {
             return false;
         }
-        if(id<=0)
-        {
+        if (id <= 0) {
             return false;
         }
         return userService.removeById(id);
@@ -116,15 +131,16 @@ public class UserController {
 
     /**
      * 判断是否为管理员
+     *
      * @param request
      * @return
      */
-    private boolean isAdmin(HttpServletRequest request){
+    private boolean isAdmin(HttpServletRequest request) {
         //仅管理员可查询
         Object userObject = request.getSession().getAttribute(USER_LOGIN_STATE);
-        User user=(User)userObject;
+        User user = (User) userObject;
 
-        return user!=null&&user.getUserRole()==ADMIN_ROLE;
+        return user != null && user.getUserRole() == ADMIN_ROLE;
     }
 
 
